a9ec6bcc065dcdb671cd53b2b4f55bbe9ef85e99
Jake Piland <jpiland@nd.edu>
2021-11-02 16:43:49 -0400

    Mostly organizational update.  - reference_code: code from the other paper and some tests that aren't being used.  - condor_logs: where condor job outputs will be stored  - nb_ files: the jupyter notebooks I use  - random py files: I need to look into these and organize them  - Note: no experiment outputs will ever be stored because even 1 checkpoint is too big
    Important new stuff:
     - nb_calibration: takes in a checkpoint file from a model, tests it, and prints out files of softmax values for graphing (train and test data only)
     - nb_graph_calibration: uses the output of nb_calibration to make calibration graphs
    
    Next to do:
     - Make one .py file will all the train functions
     - Then experiments can call that .py file
     - Submit desired experiments to condor

---
+++ train_call.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+date
+hostname
+printf "User:" ; whoami
+pwd
+
+echo
+echo "- OS Release:"
+if [ -f /etc/redhat-release ]; then
+  cat /etc/redhat-release
+elif [ -f /etc/issue ]; then
+  cat /etc/issue
+else
+  echo "OS not known"
+fi
+
+echo
+echo "- Do we have cvmfs mounted inside the container?"
+echo "ls -ld /cvmfs/cms.cern.ch"
+ls -ld /cvmfs/cms.cern.ch 2>&1
+echo
+if command -v nvidia-smi; then
+    echo "- Nvidia info:"
+    nvidia-smi
+fi
+
+echo 
+echo "- Executing python with no sgld and no aug"
+python3 train_nosgld_noaug.py 2>&1
+# use python3 instead
+# python3 pytorch_matmul.py 2>&1
+
+#echo
+#echo ------ ENVIRONMENT ----
+#env
